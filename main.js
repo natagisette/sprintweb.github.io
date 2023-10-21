ee58d02bc471613156cbe3de467d11e869b9cb5b
natagisette <148407992+natagisette@users.noreply.github.com>
2023-10-21 16:28:55 -0300

    Pagina web 2023

---
+++ main.js
@@ -0,0 +1,5 @@
+// Agrega funcionalidades JavaScript aquí
+document.querySelector("a").addEventListener("click", function (event) {
+    event.preventDefault();
+    alert("Has hecho clic en el enlace");
+});
