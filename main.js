7d795cfe36af6831bea13896786d6cb5581d5241
natagisette <148407992+natagisette@users.noreply.github.com>
2023-11-16 00:26:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,60 @@
-// Agrega funcionalidades JavaScript aquí
-document.querySelector("a").addEventListener("click", function (event) {
-    event.preventDefault();
-    alert("Has hecho clic en el enlace");
-});
+//document.querySelector("a").addEventListener("click", (event) => {
+       // event.preventDefault();
+       // alert("Has hecho clic en el enlace");
+    //});
+    const hamburguer = document.querySelector (".hamburger")
+    const menu = document.querySelector (".menu-navegacion")
+
+    console.log(menu)
+    console.log(hamburguer)
+
+    hamburguer.addEventListener('click',()=>{
+      console.log("seleccionado")
+      //  menu.classList.toggle("spread")
+    }
+    )
+   
+    
+
+
+   // btn1.addEventListener("click", function(){
+   //     menu.classList.toggle("activo");
+   // }
+   // )
+
+   // window.addEventListener('click',0=>{
+       // if(menu.classList.contains('spread')
+       //   && e.target != menu  && e.target != hamburguer ){
+
+       //     menu.classList.toggle("spread")
+  //  }
+  //  }) 
+ 
+
+
+
+
+
+
+
+
+
+    /*document.addEventListener("DOMContentLoaded", function () {
+        const hamburguer = document.querySelector(".hamburguer");
+        const menuNavegacion = document.querySelector(".menu-navegacion");
+      
+        hamburguer.addEventListener("click", function () {
+          menuNavegacion.classList.toggle("spread");
+        });
+      
+        window.addEventListener("click", function (e) {
+          if (
+            menuNavegacion.classList.contains("spread") &&
+            e.target !== menuNavegacion &&
+            e.target !== hamburguer
+          ) {
+            menuNavegacion.classList.toggle("spread");
+          }
+        });
+      });*/
+      
